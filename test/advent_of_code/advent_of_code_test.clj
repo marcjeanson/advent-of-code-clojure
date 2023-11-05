1e0341362be36db32a6a406ba7e1dac2f961aa67
Marc Jeanson <git@marcjeanson.com>
2023-11-05 20:34:25 +0100

    neil new app --name advent-of-code

---
+++ test/advent_of_code/advent_of_code_test.clj
@@ -0,0 +1,7 @@
+(ns advent-of-code.advent-of-code-test
+  (:require [clojure.test :refer :all]
+            [advent-of-code.advent-of-code :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
