4ff13b399316a27c4306930300122d982324ca3f
Marc Jeanson <git@marcjeanson.com>
2023-11-12 00:57:31 +0100

    2016 day 6

---
+++ src/advent_of_code/aoc2016/day6.clj
@@ -0,0 +1,57 @@
+(ns advent-of-code.aoc2016.day6
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
+
+(def input (line-seq (io/reader (io/resource "aoc2016/day06.txt"))))
+
+
+(defn part-1 [input]
+  (->> input
+       (apply mapv vector)
+       (mapv #(frequencies %))
+       (mapv #(sort-by val %))
+       (mapv last)
+       keys
+       (apply str)))
+
+(defn part-2 [input]
+  (->> input
+       (apply mapv vector)
+       (mapv #(frequencies %))
+       (mapv #(sort-by val %))
+       (mapv first)
+       keys
+       (apply str)))
+
+(comment
+
+  (def sample ["eedadn"
+               "drvtee"
+               "eandsr"
+               "raavrd"
+               "atevrs"
+               "tsrnev"
+               "sdttsa"
+               "rasrtv"
+               "nssdts"
+               "ntnada"
+               "svetve"
+               "tesnvt"
+               "vntsnd"
+               "vrdear"
+               "dvrsen"
+               "enarar"])
+
+
+  (part-1 sample)
+
+  (part-1 input)
+  ;; => "gebzfnbt"
+
+
+  (part-2 sample)
+  (part-2 input)
+
+
+  )
