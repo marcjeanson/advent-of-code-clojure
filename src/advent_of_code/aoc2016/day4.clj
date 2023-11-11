3e91d5b9da94ed5478231a98c72fc32352f25afb
Marc Jeanson <git@marcjeanson.com>
2023-11-11 03:15:05 +0100

    2016 Day 4 part 2

--- src/advent_of_code/aoc2016/day4.clj
+++ src/advent_of_code/aoc2016/day4.clj
@@ -17,14 +17,41 @@
           (map first)
           (apply str))))
 
-(defn part-1 [input]
+(defn valid-rooms [input]
   (->> input
-       (map #(rest (re-find #"(^[a-z-]+)(\d+)\[([a-z]+)" %)))
+       (map #(rest (re-find #"(^[a-z-]+)\-(\d+)\[([a-z]+)" %)))
        (filter (fn [[room _sector checksum]]
-                 (= (calc-checksum room) checksum)))
+                 (= (calc-checksum room) checksum)))))
+
+(defn part-1 [input]
+  (->> (valid-rooms input)
        (map fnext)
        (map #(Integer/parseInt %))
        (reduce +)))
+
+(defn decrypt-char [c shift]
+  (if (= c \-)
+    \space
+    (-> c
+        (int)
+        (- (int \a))
+        (+ shift)
+        (mod 26)
+        (+ (int \a))
+        (char))))
+
+(defn decrypt [cipertext shift]
+  (let [decrypted (->> cipertext
+                       (map #(decrypt-char % (Integer/parseInt shift)))
+                       (apply str))]
+    {:text decrypted :sector shift}))
+
+(defn part-2 [input]
+  (->> (valid-rooms input)
+       (map #(decrypt (first %) (fnext %)))
+       (filter #(re-find #"north" (:text %)))
+       first
+       :sector))
 
 (comment
   (def sample '("aaaaa-bbb-z-y-x-123[abxyz]"
@@ -32,10 +59,11 @@
                 "not-a-real-room-404[oarel]"
                 "totally-real-room-200[decoy]"))
 
-  (def room-name "aaaaa-bbb-z-y-x-")
-
+  (def room-name "aaaaa-bbb-z-y-x")
 
   (part-1 sample)
   (part-1 input)
+
+  (part-2 input)
 
   )
